ccd41c91b8f553ad05e69b040fa3dd1bb0e8fdab
Muhammad Wahaj <wahajsiddiqui07@gmail.com>
2025-11-09 21:27:19 +0500

    streamline ApiStack constructor and improve stage name handling in main.js

--- cdk/constructs/api-stack.js
+++ cdk/constructs/api-stack.js
@@ -1,30 +1,26 @@
-const { Stack, Construct, StackProps } = require('aws-cdk-lib')
+const { Stack } = require('aws-cdk-lib')
 const { Runtime, Code, Function } = require('aws-cdk-lib/aws-lambda')
 const { RestApi, LambdaIntegration } = require('aws-cdk-lib/aws-apigateway')
 
 class ApiStack extends Stack {
-    /**
-     * @param {Construct} scope
-     * @param {string} id
-     * @param {StackProps} props
-     */
-    constructor(scope, id, props) {
-        super(scope, id, props)
+  constructor(scope, id, props) {
+    super(scope, id, props)
 
-        const lambdaFunction = new Function(this, 'HandlerFunction', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: 'handler.hello',
-            code: Code.fromAsset('functions'),
-        })
+    const lambdaFunction = new Function(this, 'HandlerFunction', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'handler.hello',
+      code: Code.fromAsset('functions'),
+    })
 
-        const api = new RestApi(this, 'MyApi')
+    const api = new RestApi(this, `${props.stageName}-MyApi`, {
+      deployOptions: {
+        stageName: props.stageName
+      }
+    })
 
-        const cfnLambdaFunction = lambdaFunction.node.defaultChild
-        cfnLambdaFunction.overrideLogicalId('HandlerFunction')
-
-        const lambdaIntegration = new LambdaIntegration(lambdaFunction)
-        api.root.addMethod('GET', lambdaIntegration)
-    }
+    const lambdaIntegration = new LambdaIntegration(lambdaFunction)
+    api.root.addMethod('GET', lambdaIntegration)
+  }
 }
 
 module.exports = { ApiStack }
