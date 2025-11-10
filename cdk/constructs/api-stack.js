d2b1cf514740fd14e50c1ffba9f0c5626879f11e
Muhammad Wahaj <wahajsiddiqui07@gmail.com>
2025-11-10 22:03:11 +0500

    add DatabaseStack and integrate with ApiStack; update .gitignore and add environment setup script

--- cdk/constructs/api-stack.js
+++ cdk/constructs/api-stack.js
@@ -3,36 +3,38 @@ const { Runtime, Code, Function } = require('aws-cdk-lib/aws-lambda')
 const { RestApi, LambdaIntegration } = require('aws-cdk-lib/aws-apigateway')
 
 class ApiStack extends Stack {
-  constructor(scope, id, props) {
-    super(scope, id, props)
+    constructor(scope, id, props) {
+        super(scope, id, props)
 
-    const getIndexFunction = new Function(this, 'GetIndex', {
-      runtime: Runtime.NODEJS_18_X,
-      handler: 'get-index.handler',
-      code: Code.fromAsset('functions'),
-    })
+        const getIndexFunction = new Function(this, 'GetIndex', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'get-index.handler',
+            code: Code.fromAsset('functions'),
+        })
 
-    const getRestaurantsFunction = new Function(this, 'GetRestaurants', {
-      runtime: Runtime.NODEJS_18_X,
-      handler: 'get-restaurants.handler',
-      code: Code.fromAsset('functions'),
-      environment: {
-        default_results: '8',
-        restaurants_table: props.restaurantsTable.tableName
-      }
-    })
+        const getRestaurantsFunction = new Function(this, 'GetRestaurants', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'get-restaurants.handler',
+            code: Code.fromAsset('functions'),
+            environment: {
+                default_results: '8',
+                restaurants_table: props.restaurantsTable.tableName
+            }
+        })
 
-    const api = new RestApi(this, `${props.stageName}-MyApi`, {
-      deployOptions: {
-        stageName: props.stageName
-      }
-    })
+        props.restaurantsTable.grantReadData(getRestaurantsFunction)
 
-    const getIndexLambdaIntegration = new LambdaIntegration(getIndexFunction)
-    const getRestaurantsLambdaIntegration = new LambdaIntegration(getRestaurantsFunction)
-    api.root.addMethod('GET', getIndexLambdaIntegration)
-    api.root.addResource('restaurants').addMethod('GET', getRestaurantsLambdaIntegration)
-  }
+        const api = new RestApi(this, `${props.stageName}-MyApi`, {
+            deployOptions: {
+                stageName: props.stageName
+            }
+        })
+
+        const getIndexLambdaIntegration = new LambdaIntegration(getIndexFunction)
+        const getRestaurantsLambdaIntegration = new LambdaIntegration(getRestaurantsFunction)
+        api.root.addMethod('GET', getIndexLambdaIntegration)
+        api.root.addResource('restaurants').addMethod('GET', getRestaurantsLambdaIntegration)
+    }
 }
 
 module.exports = { ApiStack }
